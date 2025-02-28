e9b32ea9acdffe58ea8af102315457694fe1425a
bitsexploited <tspamiitesh@gmail.com>
2025-02-28 23:43:47 +0530

    Fixing the yes errors in zig

--- src/yes.zig
+++ src/yes.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer.();
+    const stdout = std.io.getStdOut().writer();
     const args = std.os.argv[1..];
 
     const output = if(args.len > 0) std.mem.span(args[0]) else "y";
